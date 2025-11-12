d961f6fa1e3adbf5eba4c72320fe5b14b8aa1643
chloeongo <chloe.ongolesono@gmail.com>
2025-11-12 10:37:29 +0100

    components oefenen

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'ToDoItem',
+}
+</script>
+
+<template>
+  <div>
+    <input type="checkbox" id="todo-item" />
+    <label for="todo-item">My todo item</label>
+  </div>
+</template>
+
+<style></style>
