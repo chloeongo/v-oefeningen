632d6a724c090947daef10c45f6af9025f3de4ab
chloeongo <chloe.ongolesono@gmail.com>
2025-11-12 11:28:58 +0100

    v-bind

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,14 +1,26 @@
 <script>
 export default {
-  name: 'ToDoItem',
+  data() {
+    return {
+      isImportant: true,
+    }
+  },
 }
 </script>
 
 <template>
   <div>
-    <input type="checkbox" id="todo-item" />
-    <label for="todo-item">My todo item</label>
+    <!-- v afkorting v-bind : -->
+    <div :class="{ myClass: isImportant }">Text example</div>
   </div>
 </template>
 
-<style></style>
+<style>
+.myClass {
+  background-color: rgb(226, 43, 128);
+  padding: 20px;
+  border-radius: 20px;
+  border: 1px solid #fff;
+  color: #fff;
+}
+</style>
